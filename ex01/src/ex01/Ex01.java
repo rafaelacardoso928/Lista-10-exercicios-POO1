d017d2110e64eb90a940b03a2dd4f2a963bb36bf
rafaelacardoso928 <cardosorafaela.2802@gmail.com>
2025-02-28 16:48:28 -0300

    ex01

---
+++ ex01/src/ex01/Ex01.java
@@ -0,0 +1,17 @@
+
+package ex01;
+
+public class Ex01 {
+
+    public static void main(String[] args) {
+    
+        Produto produto = new Produto();
+produto.setNome("teclado");
+produto.setPreco(100);
+System.out.println("nome :" + produto.getNome());
+System.out.println("preco: " + produto.getPreco());
+}
+
+    }
+    
+
---
+++ ex01/src/ex01/Produto.java
@@ -0,0 +1,25 @@
+
+package ex01;
+
+public class Produto {
+   private String nome;
+private double preco;
+public String getNome() {
+return this.nome;
+}
+public void setNome(String nome) {
+this.nome = nome;
+}
+public double getPreco() {
+return this.preco;
+}
+public void setPreco(double preco) {
+// Verificando se o preço é negativo
+if (preco < 0) {
+System.out.println("Erro: O preco nao pode ser negativo!");
+} else {
+this.preco = preco;
+}
+}
+} 
+
